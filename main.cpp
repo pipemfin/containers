58de040c7b5a3c0047a1f70686641b1366645b3f
pipemfin <pipemfin@yandex.ru>
2022-03-09 23:34:42 +0300

    add some functions

--- main.cpp
+++ main.cpp
@@ -1,30 +1,67 @@
 #include <iostream>
-//#include "vector.hpp"
-#include <vector>
+#include "vector.hpp"
+//#include <vector>
+
+//int main(int argc, char** argv)
+//{
+//    (void)argc;
+//    (void)argv;
+//    std::vector<int> abc(3);
+//    std::vector<int> def(1);
+//    std::vector<int> def2(2);
+//    std::vector<int> def3(4);
+//    std::vector<int> def4(5);
+//    std::vector<int> def5(8);
+//    std::vector<int> def6(9);
+//
+//    std::cout << "size:" << abc.size() << " ,capacity:" << abc.capacity() << std::endl;
+//    for(int i = 0; i < 10; ++i){
+//        abc.push_back(i);
+//        std::cout << "size:" << abc.size() << " ,capacity:" << abc.capacity() << std::endl;
+//    }
+//    std::cout << "size:" << abc.size() << " ,capacity:" << abc.capacity() << std::endl;
+//    std::cout << "size:" << def.size() << " ,capacity:" << def.capacity() << std::endl;
+//    std::cout << "size:" << def2.size() << " ,capacity:" << def2.capacity() << std::endl;
+//    std::cout << "size:" << def3.size() << " ,capacity:" << def3.capacity() << std::endl;
+//    std::cout << "size:" << def4.size() << " ,capacity:" << def4.capacity() << std::endl;
+//    std::cout << "size:" << def5.size() << " ,capacity:" << def5.capacity() << std::endl;
+//    std::cout << "size:" << def6.size() << " ,capacity:" << def6.capacity() << std::endl;
+//    return (0);
+//}
 
 int main(int argc, char** argv)
 {
     (void)argc;
     (void)argv;
-    std::vector<int> abc(3);
-    std::vector<int> def(1);
-    std::vector<int> def2(2);
-    std::vector<int> def3(4);
-    std::vector<int> def4(5);
-    std::vector<int> def5(8);
-    std::vector<int> def6(9);
+    ft::vector<int> abc(9, 5);
+
 
     std::cout << "size:" << abc.size() << " ,capacity:" << abc.capacity() << std::endl;
-    for(int i = 0; i < 10; ++i){
-        abc.push_back(i);
-        std::cout << "size:" << abc.size() << " ,capacity:" << abc.capacity() << std::endl;
-    }
+    for(int i = 0; i < 9; ++i)
+        std::cout << abc[i] << std::endl;
+    abc.push_back(10);
     std::cout << "size:" << abc.size() << " ,capacity:" << abc.capacity() << std::endl;
-    std::cout << "size:" << def.size() << " ,capacity:" << def.capacity() << std::endl;
-    std::cout << "size:" << def2.size() << " ,capacity:" << def2.capacity() << std::endl;
-    std::cout << "size:" << def3.size() << " ,capacity:" << def3.capacity() << std::endl;
-    std::cout << "size:" << def4.size() << " ,capacity:" << def4.capacity() << std::endl;
-    std::cout << "size:" << def5.size() << " ,capacity:" << def5.capacity() << std::endl;
-    std::cout << "size:" << def6.size() << " ,capacity:" << def6.capacity() << std::endl;
+    abc.push_back(10);
+    std::cout << "size:" << abc.size() << " ,capacity:" << abc.capacity() << std::endl;
+    std::cout << abc[8] << std::endl;
+    std::cout << "size:" << abc.size() << " ,capacity:" << abc.capacity() << std::endl;
+//    abc.pop_back();
+//    std::cout << "size:" << abc.size() << " ,capacity:" << abc.capacity() << std::endl;
     return (0);
 }
+
+//int main(int argc, char** argv)
+//{
+//    (void)argc;
+//    (void)argv;
+//    ft::vector<int> abc;
+//
+//    std::cout << "size:" << abc.size() << " ,capacity:" << abc.capacity() << std::endl;
+//
+////    for(int i = 0; i < 10; ++i){
+////        abc.push_back(i);
+////        std::cout << "size:" << abc.size() << " ,capacity:" << abc.capacity() << std::endl;
+////    }
+//
+//    return (0);
+//}
--- vector.hpp
+++ vector.hpp
@@ -4,36 +4,48 @@
 #include <stddef.h>
 #include <memory>
 #include <cmath>
+#include <iostream>
 namespace ft {
 
     template<typename T, class allocator_type = std::allocator<T> >
     class vector{
     private:
-        size_t          sz;
-        size_t          cpcty;
-        T*              ptr;
-        allocator_type  alloc;
+        size_t                  sz;
+        size_t                  cpcty;
+        T*                      ptr;
+        allocator_type          alloc;
+
 
     public:
+        struct iterator {
+        };
+
         vector (const allocator_type& alloc = allocator_type()) {
             this->sz = 0;
             this->cpcty = 0;
             this->ptr = NULL;
+            this->alloc = alloc;
         }
 
-        vector (size_t n, const T& val = T(),
-                const allocator_type& alloc = allocator_type()) {
+        vector (size_t n, const T& val = T(), const allocator_type& alloc = allocator_type()) {
             this->sz = n;
             this->cpcty = n;
-            this->ptr = alloc.allocate(n);
-            alloc.construct(ptr, val);
+            this->alloc = alloc;
+            this->ptr = this->alloc.allocate(n);
+            for (size_t cnt = 0; cnt < sz; cnt++) {
+                this->alloc.construct(&this->ptr[sizeof(T) * cnt], val);
+            }
         }
 
-        template <class InputIterator>
-        vector (InputIterator first, InputIterator last,
-                const allocator_type& alloc = allocator_type());
+//        template <class InputIterator>
+//                vector (InputIterator first, InputIterator last,
+//                        const allocator_type& alloc = allocator_type());
 
-        vector (const vector& x);
+
+
+
+
+//        vector (const vector& x);
 
         size_t size() const{
             return this->sz;
@@ -47,32 +59,16 @@ namespace ft {
             return this->cpcty;
         }
 
-        void reserve (size_t n) {
-            T* temp = NULL;
-            T* for_del = NULL;
-            if (n > this->cpcty) {
-                temp = this->alloc.allocate(n);
-                for(size_t cnt = 0; cnt < sz; cnt++)
-                    temp[cnt](ptr[cnt]);
-                for_del = this->ptr;
-                this->ptr = temp;
-                for(size_t cnt = sz; cnt > 0; --cnt)
-                    for_del[cnt]->~T();
-                delete[] for_del;
-            }
-        }
-
         void resize (size_t n, T val = T()) {
             if (n < this->sz) {
-                for (size_t tmp = this->sz; tmp > n; --tmp)
-                    alloc.destroy(ptr[sizeof(T) * tmp]);
-                this->sz = n;
+                for (; this->sz > n; this->sz--)
+                    this->alloc.destroy(&ptr[sizeof(T) * this->sz]);
             }
             else if (n > this->cpcty) {
                 reserve(n);
             }
             else if (n > this->sz)
-                for(; this->sz < n;++(this->sz))
+                for(; this->sz < n;++this->sz)
                     ptr[sz - 1] = T(val);
         }
 
@@ -82,12 +78,56 @@ namespace ft {
             return false;
         }
 
-        & operator[] (size_t n) {
+        // + изменяет размер вектора до размера n, если текущий размер >= n, то ничего не делает, вроде норм написано
+        void reserve (size_t n) {
+            T* temp_ptr = NULL;
+            T* for_del = NULL;
+            size_t new_cpcty = this->cpcty * 2;
+            std::cout << "here11" << std::endl;
+            if (n > this->cpcty) {
+                if (n > new_cpcty) {
+                    new_cpcty = n;
+                }
+                std::cout << "here12" << new_cpcty << std::endl;
+                temp_ptr = this->alloc.allocate(new_cpcty);
+                std::cout << "here121" << std::endl;
+                for (size_t cnt = 0; cnt < this->sz; cnt++) {
+                    std::cout << "here122" << std::endl;
+                    this->alloc.construct(&temp_ptr[sizeof(T) * cnt], this->ptr[sizeof(T) * cnt]);
+                }
+                std::cout << "here13" << std::endl;
+                for_del = this->ptr;
+                this->ptr = temp_ptr;
+                for (size_t cnt = this->sz; cnt > 0; --cnt) {
+                    this->alloc.destroy(&for_del[sizeof(T) * sz]);
+                }
+                std::cout << "here14" << std::endl;
+                this->alloc.deallocate(for_del, this->cpcty);
+                std::cout << "here15" << std::endl;
+                this->cpcty = new_cpcty;
+            }
+        }
+
+        T operator[] (size_t n) {
             return ptr[sizeof(T) * n];
         }
 
-        const &T operator[] (size_t n) {
-            return ptr[sizeof(T) * n];
+        // + вставляет элемент поданный в качестве аргумента в конце массива
+        void push_back (const T& val) {
+            std::cout << "here" << std::endl;
+            if (this->sz == this->cpcty) {
+                reserve(this->cpcty + 1);
+            }
+            std::cout << "here1" << std::endl;
+            this->ptr[sizeof(T) * this->sz] = val;
+            this->sz++;
+        }
+
+        // + удаляет элемент с конца массива, уничтожая его
+        void pop_back() {
+            if (this->sz > 0) {
+                this->alloc.destroy(&this->ptr[sizeof(T) * --this->sz]);
+            }
         }
 
         ~vector() {};
